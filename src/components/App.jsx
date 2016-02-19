bd1567cb97b01af44a3e253292a29152fb0c801a
Charles F. Munat <charles.munat@gmail.com>
2016-02-19 13:54:04 +1300

    Ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = ({}, {}) => {
-  return <div className='app'></div>
+class App extends Component {
+
+  render () {
+    return <div className='app'></div>
+  }
 }
 
 export default App
