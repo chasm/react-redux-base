7b5aa862a9448015f3d233e7c6709472827d2a28
Charles F. Munat <charles.munat@gmail.com>
2016-02-19 17:48:14 +1300

    Comments added to code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react' // import React and Component from inside React
 
+import Board from './Board.jsx'  // import our own Board component from this folder
+
+// Create our top-level App component by subclassing React's Component
 class App extends Component {
 
+  // React calls render to render our component to the virtual DOM
+  // Here we just return a <div class="app"> with whatever our Board renders
+  // inside of it
   render () {
-    return <div className='app'></div>
+    return <div className='app'>
+      <Board/>
+    </div>
   }
 }
 
-export default App
+export default App // export the App so we can import it into index.js
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react'         // import React
+import ReactDOM from 'react-dom'  // import the ReactDOM (needed here only)
 
-import App from './components/App.jsx'
+import App from './components/App.jsx' // import our own App component
 
-const app = document.createElement('div')
+const app = document.createElement('div') // create a <div> element
 
-document.body.appendChild(app)
+document.body.appendChild(app) // append that <div> element to the <body>
 
-ReactDOM.render(<App/>, app)
+ReactDOM.render(<App/>, app) // render our App into the <div> element
