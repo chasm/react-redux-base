8e40a317f7d5a89f91ccf3731bdb6d9096b450f6
Charles F. Munat <charles.munat@gmail.com>
2016-02-26 14:56:16 +1300

    Set first handlers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = ({}, {}) => {
-  return <div>App!</div>
+function log(name, e) {
+  console.log(name, JSON.parse(JSON.stringify(e)))
+}
+
+class App extends Component {
+
+  handleClick (e) {
+    log('click', e)
+  }
+
+  handleCopy (e) {
+    log('copy', e)
+  }
+
+  render () {
+    return <div
+        onClick={this.handleClick.bind(this)}
+        onCopy={this.handleCopy.bind(this)}
+      >App!</div>
+  }
 }
 
 export default App
