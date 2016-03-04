30ccdff6e27161bf542f411f9111eb42d448f652
Charles F. Munat <charles.munat@gmail.com>
2016-03-04 14:05:37 +1300

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './components/App.jsx'
 
 const app = document.createElement('div')
 
+app.setAttribute('class', 'container')
+
 document.body.appendChild(app)
 
 ReactDOM.render(<App/>, app)
