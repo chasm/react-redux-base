cddd754690394b0b6c24bd68ffb900a1ad1e17ef
Charles F. Munat <charles.munat@gmail.com>
2016-03-04 13:45:15 +1300

    Ready to start

--- test/tests.js
+++ test/tests.js
@@ -1,21 +1,22 @@
+/* global describe it */
+
 import React from 'react'
-import ReactDOM from 'react-dom'
-import TestUtils from 'react-addons-test-utils'
+// import ReactDOM from 'react-dom'
+// import TestUtils from 'react-addons-test-utils'
 
 import chai, { expect } from 'chai'
 import chaiEnzyme from 'chai-enzyme'
 
 chai.use(chaiEnzyme())
 
-import { mount, render, shallow } from 'enzyme'
+import { shallow } from 'enzyme' // mount, render
 
 import App from '../src/components/app.jsx'
 
 describe('<App/>', () => {
-
   it('renders a div containing "App"', () => {
     const wrapper = shallow(<App/>)
 
-    expect(wrapper.find('div')).to.have.className('app')
+    expect(wrapper.find('div.app')).to.have.className('app')
   })
 })
